0515aaeea2873d4c935446090867fc1c567c831e
JerryChen <c_Up1d@outlook.com>
2022-09-30 16:04:38 +0800

    Add autoAttendance Script

---
+++ auto.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+import datetime
+import getpass
+import os
+import time
+
+import json
+import re
+import requests
+import urllib3
+from apscheduler.schedulers.blocking import BlockingScheduler
+from halo import Halo
+
+
+class DaKa(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.login_url = "http://ca.its.csu.edu.cn/Home/Login/215"
+        self.validate_url = "https://wxxy.csu.edu.cn/a_csu/api/sso/validate"
+        self.base_url = "https://wxxy.csu.edu.cn/ncov/wap/default/index"
+        self.save_url = "https://wxxy.csu.edu.cn/ncov/wap/default/save"
+        self.info = None
+        self.sess = requests.Session()
+
+    def login(self):
+        """Login to CSU platform and verify"""
+        data1 = {
+            "userName": self.username,
+            "passWord": self.password,
+            "enter": 'true'
+        }
+        res2 = None
+        try:
+            res2 = self.sess.post(url=self.login_url, data=data1)
+        except:
+            print("无法连接信网中心")
+        if res2 is None:
+            print("请检查账号密码是否正确")
+
+        regex = r'tokenId.*value="(?P<tokenId>\w+)".*account.*value="(?P<account>\w+)".*Thirdsys.*value="(' \
+                r'?P<Thirdsys>\w+)" '
+        re_result = re.search(regex, res2.text)
+        data2 = {
+            "tokenId": re_result["tokenId"],
+            "account": re_result["account"],
+            "Thirdsys": re_result["Thirdsys"]
+        }
+        try:
+            self.sess.post(self.validate_url, data=data2)
+        except:
+            print("无法通过信网中心认证")
+        return self.sess
+
+    def get_info(self, html=None):
+        """Get hitcard info, which is the old info with updated new time."""
+        if not html:
+            urllib3.disable_warnings()
+            res = self.sess.get(self.base_url, verify=False)
+            html = res.content.decode()
+
+        jsontext = re.findall(r'def = {[\s\S]*?};', html)[0]
+        jsontext = eval(jsontext[jsontext.find("{"):jsontext.rfind(";")].replace(" ", ""))
+
+        geo_text = jsontext['geo_api_info']
+        geo_text = geo_text.replace("false", "False").replace("true", "True")
+        geo_obj = eval(geo_text)['addressComponent']
+        area = geo_obj['province'] + " " + geo_obj['city'] + " " + geo_obj['district']
+        name = re.findall(r'realname: "([^\"]+)",', html)[0]
+        number = re.findall(r"number: '([^\']+)',", html)[0]
+
+        new_info = jsontext.copy()
+        new_info['name'] = name
+        new_info['number'] = number
+        new_info['area'] = area
+        new_info["date"] = self.get_date()
+        new_info["created"] = round(time.time())
+        self.info = new_info
+        return new_info
+
+    def get_date(self):
+        today = datetime.date.today()
+        return "%4d%02d%02d" % (today.year, today.month, today.day)
+
+    def post(self):
+        """Post the hitcard info"""
+        res = self.sess.post(self.save_url, data=self.info)
+        return json.loads(res.text)
+
+
+def main(username, password):
+    print("\n[Time] %s" % datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    print("🚌 打卡任务启动")
+    spinner = Halo(text='Loading', spinner='dots')
+    spinner.start('正在新建打卡实例...')
+    dk = DaKa(username, password)
+    spinner.succeed('已新建打卡实例')
+
+    spinner.start(text='登录到中南大学信息门户...')
+    dk.login()
+    spinner.succeed('已登录到中南大学信息门户')
+
+    spinner.start(text='正在获取个人信息...')
+    dk.get_info()
+    spinner.succeed('%s %s同学, 你好~' % (dk.info['number'], dk.info['name']))
+
+    spinner.start(text='正在为您打卡打卡打卡')
+    res = dk.post()
+    if str(res['e']) == '0':
+        spinner.stop_and_persist(symbol='🦄 '.encode('utf-8'), text='已为您打卡成功！')
+    else:
+        spinner.stop_and_persist(symbol='🦄 '.encode('utf-8'), text=res['m'])
+
+
+if __name__ == "__main__":
+    if os.path.exists('./config.json'):
+        configs = json.loads(open('./config.json', 'r', encoding='utf-8').read())
+        username = configs["username"]
+        password = configs["password"]
+        hour = configs["schedule"]["hour"]
+        minute = configs["schedule"]["minute"]
+    else:
+        username = input("👤 中南大学学工号: ")
+        password = getpass.getpass('🔑 中南大学信息门户密码: ')
+        print("⏲ 请输入定时时间（默认每天7:05）")
+        hour = input("\thour: ") or 7
+        minute = input("\tminute: ") or 5
+
+    # Schedule task
+    scheduler = BlockingScheduler()
+    scheduler.add_job(main, 'cron', args=[username, password], hour=hour, minute=minute)
+    print('⏰ 已启动定时程序，每天 %02d:%02d 为您打卡' % (int(hour), int(minute)))
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
